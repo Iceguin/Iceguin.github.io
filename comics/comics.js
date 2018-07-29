ae200acc45d6a2c98d1737dea226ecb50293fa04
Iceguin <xbird.loverx@gmail.com>
2018-07-29 15:39:12 -0400

    eh

--- comics/comics.js
+++ comics/comics.js
@@ -2,17 +2,15 @@ $.ajax({
   type: 'GET',
   url: 'https://www.gocomics.com/calvinandhobbes/2018/07/29',
   contentType: 'text/html',
-  /*dataType:'jsonp',*/
+  dataType:'text/html',
   responseType:'text/html',
   xhrFields: {
     withCredentials: false
   },
   headers: {
-    'Access-Control-Allow-Credentials' : false,
     'Access-Control-Allow-Origin':'*',
     'Access-Control-Allow-Methods':'GET',
-    'Access-Control-Allow-Headers':'text/html',
-    'Content-Type': 'text/html',
+    'Access-Control-Allow-Headers':'text/html'
   },
   success: function(data) {
     console.log(data);
